4c75e6707a59fcc79002aff1c8643a4f01b04bb6
Binlong Li <herbert19lee@gmail.com>
2011-06-25 10:54:20 -0400

    Adding median filter to the result, beginning of Opening/Closing

--- 15PostProcessing/pPro_celltrack.m
+++ 15PostProcessing/pPro_celltrack.m
@@ -1,13 +1,45 @@
-function pPro_celltrack(fg, isVis)
+function pPro_celltrack(fg, isVis, isSpec)
 
 nframes = size(fg, ndims(fg));
-%% 
 if isVis
-    close all; 
+    display('fg Visualization');
+    visSubFun(fg, isSpec);
+end
+
+% Median Filter
+fgVideo = zeros(size(fg));
+for i = 1 : nframes
+    tfg = fg(:, :, i);
+    tfg = medfilt2(tfg, [3 3]);
+    fgVideo(:, :, i) = tfg;
+end
+if isVis
+    display('fgVideo Visualization');
+    visSubFun(fgVideo, isSpec);
+end
+
+% closingVideo = logic(fgVideo);
+% se90 = strel('diamond', 3, 90);     se0 = strel('diamond', 3, 0);
+% for i = 1 : nframes
+%     tfg = fgVideo(:, :, i);
+    
+
+
+
+
+function visSubFun(fg, isSpec)
+
+nframes = size(fg, ndims(fg));
+close all;
+if isSpec
     startShowFrame = 44;    endShowFrame = 50;
-    for i = startShowFrame : endShowFrame
-        imshow(uint8(fg(:, :, i)));
-        title(['Frame ' int2str(i)]);
-        pause(1/11);        
-    end
+else
+    startShowFrame = 1;     endShowFrame = nframes;
+end
+
+for i = startShowFrame : endShowFrame
+    tfg = fg(:, :, i);
+    imshow(uint8(tfg));
+    title(['Frame ' int2str(i)]);
+    pause(1/11);
 end
