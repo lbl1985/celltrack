8b96c868212e663d0b573b89a0b036bac540e7bf
Binlong Li <herbert19lee@gmail.com>
2011-06-25 17:04:20 -0400

    Functionalize method bkgd_with_dynamics

--- 90support/ideaShow_celltrack.m
+++ 90support/ideaShow_celltrack.m
@@ -3,4 +3,34 @@ function varargout = ideaShow_celltrack(varargin)
 method = varargin{1};
 switch method
     case 'bkgd_with_dynamics'
+        [fg srcdirImg filenamesImg fgVideo combineImage STATSBatch cellID] = varin2out(varargin{2:end});
+        nframes = size(fg, ndims(fg));
+        fig = figure(1);
+        for t = 1 : nframes
+            subplot(2, 3, 1);   imshow(uint8(imread([srcdirImg filenamesImg{t}])));
+            title(['Origin Frame ' num2str(t)]);
+            subplot(2, 3, 2); imshow(fg(:, :, t));
+            title('bkgd Sub Res');
+            subplot(2, 3, 3); imshow(fgVideo(:, :, t));
+            title('Median Filter Res');
+            subplot(2, 3, 4); imshow(combineImage(:, :, t));
+            title('Combine Images');
+            %     subplot(2, 3, 5); vec = vecBatch{t};
+            %     plot(vec(:, 1), vec(:, 2), 'ro');    axis([1 size(fg(:, :, 1), 2) 1 size(fg(:, :, 1), 1)]);
+            %     title('Region Centroids Locations');
+            subplot(2, 3, 6);   imshow(fgVideo(:, :, t));
+            if cellID(t) > 0
+                STATS = STATSBatch{t};
+                hold on;
+                for i = 1 : length(STATS)
+                    tBlob = STATS(i);
+                    cellBoundingShow(tBlob, cellID(t), fig);
+                end
+                title(['Cell ID ' num2str(cellID(t))]);
+                hold off
+            else
+                title('NO cell detected');
+            end
+            pause(1/11);
+        end
 end
