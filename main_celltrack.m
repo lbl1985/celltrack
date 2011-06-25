66f59d7bc4ed0825533be5e064f0dc94c6e7a479
Binlong Li <herbert19lee@gmail.com>
2011-06-25 08:25:36 -0400

    start post processing, add isVis section for Visualization of bkgd subtraction result in post processing section

--- main_celltrack.m
+++ main_celltrack.m
@@ -2,10 +2,15 @@
 % main_celltrackin will be the general all-in-one console for the whole project. 
 % Adding output argument: fg for CellTrajectory Function.
 % Binlong Li    25 June 2011    07:51AM
+
+% Parameter Setting Section
 close all; 
 % method = 'MEAN';
 method = 'MoG';
 isSliding = 'OFF';
+% Recording 
+debugRecord = 0;
+isVis = 1;
 switch isSliding
     case 'OFF'
         if strcmp(method, 'MoG')
@@ -14,7 +19,7 @@ switch isSliding
             nd = 1;
         end
         for id = 8
-            fg = CellTrajectory(id, nd, method);
+            fg = CellTrajectory(id, nd, method, debugRecord);
         end
         
     case 'ON'
@@ -30,3 +35,13 @@ end
 
 % Post Processing, median filter and close-opening operations.
 nframes = size(fg, ndims(fg));
+%% 
+if isVis
+    close all; 
+    startShowFrame = 44;    endShowFrame = 50;
+    for i = startShowFrame : endShowFrame
+        imshow(uint8(fg(:, :, i)));
+        title(['Frame ' int2str(i)]);
+        pause(1/11);        
+    end
+end
