9182b48f559ebde43551dee8b5b29ea264f6363d
Binlong Li <herbert19lee@gmail.com>
2011-06-25 16:30:45 -0400

    Developing not finished yet. Still need some denoise and boundingBox calculation operations

--- 21CellIDGen/cellIDGen.m
+++ 21CellIDGen/cellIDGen.m
@@ -1,16 +1,19 @@
 function varargout = cellIDGen(degreeVideo, STATSBatch)
 ID = 0;
+
 nframes = length(degreeVideo);
+cellID = zeros(nframes, 1);
 % if degreeVideo is not zero, we consider it as cell showing up at that
 % frame.
 isCell = degreeVideo > 0;
 for t = 1 : nframes
     if isCell(t)
-        Bounding = structField2Vector(STATSBatch{t}, 'Centroid');
+%         Bounding = structField2Vector(STATSBatch{t}, 'Centroid');
 %         cellBounding = 
         if ~isCell(t - 1)
             ID = ID + 1;
         end
+        cellID(t) = ID;
     end
 end
 
