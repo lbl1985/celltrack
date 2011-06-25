214ed70f4a8434fece6c983bd408833ca8e64bc2
Binlong Li <herbert19lee@gmail.com>
2011-06-25 16:03:53 -0400

    output more varables. degreeVideo combineImage vecBatch in varargout mode

--- 16HankelDecision/dynamicsVideo.m
+++ 16HankelDecision/dynamicsVideo.m
@@ -1,9 +1,14 @@
 % Working Log:
 % Intermediate function between main_celltrack and dynamicsCheck.
 % Binlong Li    25 June 2011    11:11AM
-function degreeVideo = dynamicsVideo(fgVideo)
+function varargout = dynamicsVideo(fgVideo)
 nframes = size(fgVideo, ndims(fgVideo));
 degreeVideo = zeros(nframes, 1);
+combineImage = zeros(size(fgVideo));
+vecBatch = cell(nframes, 1);
 for i = 2 : nframes - 1
-    degreeVideo(i) = dynamicsCheck(fgVideo, i);
+    [degreeVideo(i) combineImage(:, :, i) vecBatch{i}] = dynamicsCheck(fgVideo, i);
 end
+varargout{1} = degreeVideo;
+varargout{2} = combineImage; 
+varargout{3} = vecBatch;
