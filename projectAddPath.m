76eaf65f2c6f685cb0298315456fc52071e49ff5
Binlong Li <herbert19lee@gmail.com>
2011-07-13 14:24:35 -0400

    modify the AddPath function, instead of adding multiple folders. we now add the whole gen folders, then remove the ones we don't really want

--- projectAddPath.m
+++ projectAddPath.m
@@ -2,15 +2,19 @@ function projectAddPath(startingPath, projectName)
 
 switch projectName
     case 'celltrack'
-        addpath(genpath(fullfile(startingPath, '01database')));
-        addpath(genpath(fullfile(startingPath, '10BackgroundSubtraction')));
-        addpath(genpath(fullfile(startingPath, '15PostProcessing')));
-        addpath(genpath(fullfile(startingPath, '16HankelDecision')));
-        addpath(genpath(fullfile(startingPath, '20Tracking')));
-        addpath(genpath(fullfile(startingPath, '21CellIDGen')));
-        addpath(fullfile(startingPath, '90support'));
-        addpath(genpath(fullfile(startingPath, '90support', 'CvBSLib')));
-        addpath(genpath(fullfile(startingPath, '90support', 'mmread')));
-        addpath(genpath(fullfile(startingPath, '90support', 'mmwrite')));
+        addpath(genpath(fullfile(startingPath)));
+        rmpath(genpath(fullfile(startingPath, '90support', 'TFOCS_v1', 'demo')));
+        rmpath(genpath(fullfile(startingPath, '90support', 'TFOCS_v1', 'TFOCS')));
         addpath(fullfile(startingPath, '90support', 'TFOCS_v1', 'TFOCS'));
+%         addpath(genpath(fullfile(startingPath, '01database')));
+%         addpath(genpath(fullfile(startingPath, '10BackgroundSubtraction')));
+%         addpath(genpath(fullfile(startingPath, '15PostProcessing')));
+%         addpath(genpath(fullfile(startingPath, '16HankelDecision')));
+%         addpath(genpath(fullfile(startingPath, '20Tracking')));
+%         addpath(genpath(fullfile(startingPath, '21CellIDGen')));
+%         addpath(fullfile(startingPath, '90support'));
+%         addpath(genpath(fullfile(startingPath, '90support', 'CvBSLib')));
+%         addpath(genpath(fullfile(startingPath, '90support', 'mmread')));
+%         addpath(genpath(fullfile(startingPath, '90support', 'mmwrite')));
+%         addpath(fullfile(startingPath, '90support', 'TFOCS_v1', 'TFOCS'));
 end
