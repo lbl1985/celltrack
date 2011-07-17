0a9a46c3e507554dd08d3ea19a5e59fa7814321a
Binlong Li <herbert19lee@gmail.com>
2011-07-17 10:11:23 -0400

    mogPrepareFunc added. fAlphaT is dependent, both fAlphaT and mogVideo with repmat n are created in mogPrepareFunc. bkgd_subtraction_MoG function is also created

--- 90support/@videoClip/videoClip.m
+++ 90support/@videoClip/videoClip.m
@@ -15,7 +15,12 @@ classdef videoClip < handle
     end
     
     properties (SetAccess = public)
-      
+      T = 300;              fTb = 3 * 3;   
+      nd = 4;               
+    end
+    
+    properties (Dependent = true, SetAccess = private)
+        fAlphaT
     end
     
     methods 
@@ -31,9 +36,9 @@ classdef videoClip < handle
             end
         end
             
-        function obj = read_fg_MoG(videoPath, videoName)
-            load(fullfile(videoPath, videoName));
-            obj.foreGround_MoG = fg;
+        function obj = bkgd_subtraction_MoG(obj)
+            mogVideo = mogPrepareFunc(obj);
+            obj.foreGround_MoG = bkgd_methods.mog(mogVideo, 1/obj.T, obj.fTb, obj.nd);            
         end
         
         function read_fg_RPCA(obj)
@@ -47,7 +52,19 @@ classdef videoClip < handle
     end
     
     methods % supporting functions
+        function mogVideo = mogPrepareFunc(obj)
+            if(obj.T ~= 0)
+                obj.fAlphaT = 1/ obj.T;
+            end
+            if(ndims(obj.origVideo) == 3)
+                repmat_para = [ 1 1 obj.nd];
+            else
+                repmat_para = [1 1 1 obj.nd];
+            end
+            mogVideo = repmat(obj.origVideo, repmat_para);
+        end
             
+        
     end
         
     
