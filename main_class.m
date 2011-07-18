debafe5834357701f10fa9f4aa7c9c82012fe4e1
Binlong Li <herbert19lee@gmail.com>
2011-07-18 13:16:29 -0400

    prepare to cooperate with batchRun_object results

--- main_class.m
+++ main_class.m
@@ -6,13 +6,18 @@ projectAddPath(workingpath, 'celltrack');
 datapath = fullfile(workingpath, '01database', 'vivo');
 [datapath videoName n] = rfdatabase(datapath, [], '.avi');
 
-for id = [1 4 6:11 13:17]
-% for id = 7    
-    vt = cellCountClip(datapath, videoName{id});
-    vt.read_Video();
-    vt.bkgd_subtraction_MoG();
-    vt.bkgd_subtraction_rpca();
+% bkgd subtraction section
+% for id = [1 4 6:11 13:17]
+% % for id = 7    
+%     vt = cellCountClip(datapath, videoName{id});
+%     vt.read_Video();
+%     vt.bkgd_subtraction_MoG();
+%     vt.bkgd_subtraction_rpca();
+% 
+%     vt.saveData();
+%     clear vt
+% end
 
-    vt.saveData();
-    clear vt
-end
+%% cellCount Section
+for id = [ 1 4 6:11 13:17]
+    
