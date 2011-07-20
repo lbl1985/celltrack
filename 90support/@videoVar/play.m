1709ccb4c42e048ef81275f32dd5d88d034ebc0f
Binlong Li <herbert19lee@gmail.com>
2011-07-20 15:39:35 -0400

    start of videoVar programming

---
+++ 90support/@videoVar/play.m
@@ -0,0 +1,7 @@
+function [ output_args ] = play( input_args )
+%support function for videoVar class
+%   play the videoVar data
+
+
+end
+
---
+++ 90support/@videoVar/videoVar.m
@@ -0,0 +1,30 @@
+classdef videoVar < handle
+    %Include all basic functionality may applied to a video varable in
+    %Matalb
+    %   properties  : 
+    %       Data
+    %       siz
+    %       ndim
+    %   methods      
+    %       play    : play the Data
+    %       save    : save the video as input name
+    
+    properties
+        Data        
+        siz 
+        ndim
+        nFrame
+    end
+    
+    methods
+        function obj = videoVar(inputData)
+            if nargin > 0
+                obj.Data = inputData;
+                obj.siz = size(obj.Data);
+                obj.ndim = length(obj.siz);
+                obj.nFrame = obj.siz(obj.ndim);
+            end
+        end        
+    end    
+end
+
