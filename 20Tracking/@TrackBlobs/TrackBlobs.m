e904db5554dab54859b9fd15666159fb1aa95883
Binlong Li <herbert19lee@gmail.com>
2011-07-23 14:37:55 -0400

    TrackBlob Class created

---
+++ 20Tracking/@TrackBlobs/TrackBlobs.m
@@ -0,0 +1,26 @@
+classdef TrackBlobs
+    %TRACKBLOBS Summary of this class goes here
+    %   Detailed explanation goes here
+    
+    properties
+        fg        
+        nSeg = []
+
+        DB = {};
+        ID = 1;        
+    end
+    
+    properties (SetAccess = public)
+        record = 0;
+        areaThreshold = 5;
+        videoName = [];
+    end
+    
+    methods
+        function obj = TrackBlobs(inputForeGround)
+            obj.fg = videoVar(inputForeGround);   
+            obj.nSeg = zeros(obj.fg.nFrame, 1);
+        end
+    end
+end
+
---
+++ 20Tracking/@TrackBlobs/blobTrackingFunc.m
@@ -0,0 +1,71 @@
+function  blobTrackingFunc(obj)
+%BLOBTRACKINGFUNC Summary of this function goes here
+%   Detailed explanation goes here
+
+if obj.record
+    moviefile = [obj.videoName{id}(1:end - 4) '_blobTracking.avi']; framerate = 5;
+    aviobj = avifile(moviefile, 'fps', framerate', 'compression', 'none');
+end
+
+for t = 1 : obj.fg.nFrame
+    Ifilt = obj.fg.Data(:, :, t);    
+    rectShow = figure(1); imshow(Ifilt, 'border', 'tight');
+    STATS = regionprops(Ifilt>0);
+    obj.nSeg(t) = length(STATS);
+    
+    % In order to get rid of mass bg subtraction problem in the first
+    % several frames, need to do DB clean up after 20 frames
+    if t == 1
+        % Well, don't need t, and STATS at all. Just to fit in the
+        % interface.
+        obj.DB = databaseFuncCell(t, obj.DB, 'cleanUpBeginning', STATS);
+    end
+    
+    if obj.nSeg(t) ~= 0
+        % timeslot is varable to same information at frame t.
+        % with all fields in STATS plus trakcingID;
+        %         timeslot = STATS;
+        % if Area is less than areaThreshold Pixels, set the Area equals to
+        % 0
+        a = structField2Vector(STATS, 'Area');
+        %         timeslot(a < areaThreshold).Area =  0;
+        % Find the index for all blobs with area larger than the
+        % areaThreshold
+        %         a = structField2Vector(timeslot, 'Area');
+        ind = find(a >= obj.areaThreshold);
+        for i = 1 : length(ind)
+            tBlob = STATS(ind(i));
+            if isempty(obj.DB)
+                obj.DB = databaseFuncCell(t, obj.DB, 'add', tBlob, obj.ID);
+                cellBoundingShow(tBlob, obj.ID, rectShow);
+                obj.ID = obj.ID + 1;
+            else
+                idQuery = databaseFuncCell(t, obj.DB, 'search', tBlob);
+                if idQuery ~= 0
+                    obj.DB = databaseFuncCell(t, obj.DB, 'update', tBlob, idQuery);
+                    cellBoundingShow(tBlob, idQuery, rectShow);
+                else
+                    obj.DB = databaseFuncCell(t, obj.DB, 'add', tBlob, obj.ID);
+                    cellBoundingShow(tBlob, obj.ID, rectShow);
+                    obj.ID = obj.ID + 1;
+                end
+            end
+        end
+    end
+    
+    if obj.record
+        frame = getframe(gcf);
+        aviobj = addframe(aviobj, frame);
+    end
+    display(['Frame ' num2str(t)]);
+end
+
+if obj.record
+    aviobj = close(aviobj);
+end
+
+
+
+
+end
+
