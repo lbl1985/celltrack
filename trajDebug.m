a7a68c8c69fdb5e6b241ab3030994359b76224d4
Binlong Li <herbert19lee@gmail.com>
2011-07-25 23:43:04 -0400

    clean up the trajDebug script

--- trajDebug.m
+++ trajDebug.m
@@ -2,12 +2,5 @@ clear;
 main_class;
 trackBlobsObj = TrackBlobs(blobDetector.inputVideoData);
 trackBlobsObj.OpenClosingProcessFunc();
-% fgAfterOpenClosing = OpenClosingProcess(blobDetector.inputVideoData);
-% trackBlobsObj = TrackBlobs(blobDetector.inputVideoData);
-% trackBlobsObj.fgAfterClosing = videoVar(fgAfterOpenClosing);
-% trackBlobsObj.OpenClosingProcessFunc();
 trackBlobsObj.blobTrackingFunc();
 trackBlobsObj.playTrackingBlobs();
-% pickCellObj = pickCell(blobDetector.blobCellFrameVideo);
-
-% pickCellObj.pickCellPerFrame
