2d39a2c1c2f2b8e186de1c55f5e167e28d2dcce2
Binlong Li <herbert19lee@gmail.com>
2011-07-27 16:37:16 -0400

    Dynamics merging is not working very well. Read to modify the logic for function: findDynamcisIdenticalInSameFrame

--- 20Tracking/@TrackBlobs/DBMergeDynamics.m
+++ 20Tracking/@TrackBlobs/DBMergeDynamics.m
@@ -1,9 +1,10 @@
 function DBMergeDynamics(obj)
     % length of db is dynamically changed according to merge operation
-    dbIndex = 1;
+    dbIndex = 1;    
     while dbIndex < length(obj.DB) - 1        
         nEntry = length(obj.DB{dbIndex});        
-        if nEntry < 3            
+        if nEntry < 2   
+%             obj.DBSortByFrame;
             findDynamcisIdenticalInSameFrame(obj, dbIndex);                        
         end
         dbIndex = dbIndex + 1;
@@ -13,20 +14,48 @@ end
 function findDynamcisIdenticalInSameFrame(obj, dbIndex)
     queryEntry = obj.DB{dbIndex};
     testIndex = dbIndex + 1;
-    while queryEntry.timeIDX(end) >= obj.DB{testIndex}.timeIDX(1)
+%     timeNow = queryEntry.timeIDX;
+%     testIndex = unique(structField2Vector(obj.DBbyFrames{timeNow}, 'ID'));
+    while testIndex < length(obj.DB) && ...
+            queryEntry.timeIDX(end) >= obj.DB{testIndex}.timeIDX(1)        
         testEntry = obj.DB{testIndex};
-        isAtBegin = checkIsConsecutiveTrajectories(obj, queryEntry, testEntry);
-        if isAtBegin == 1
-            tmpCoordinateArray = [queryEntry.timeIDX' testEntry.timeIDX'];
-            hankelWindowSize = calculateHankelWindowSize(tmpCoordinateArray);
-            kHankel = hankelConstruction(tmpCoordinateArray, hankelWindowSize);
+        isBegin = checkIsBegin(queryEntry, testEntry);
+        if isBegin
+            kHankel = DynamicsHankelConstruction(queryEntry, testEntry);
             [~, S, ~] = svd(kHankel);
-            isDynamicsQualify = checkDynamics(S);
-        end
-        if isDynamicsQualify
-            obj.mergeTrajectory(obj, dbIndex, testIndex);
+            isDynamicsQualify = checkDynamics(diag(S));
+
+            if isDynamicsQualify
+                obj.mergeTrajectory(dbIndex, testIndex);
+%                 obj.DBSrotByFrame;
+            else
+                testIndex = testIndex + 1;
+            end
         else
             testIndex = testIndex + 1;
         end
     end
 end
+
+function isBegin = checkIsBegin(queryEntry, testEntry)
+    isBegin = queryEntry.timeIDX(end) == testEntry.timeIDX(1);
+end
+
+function kHankel = DynamicsHankelConstruction(queryEntry, testEntry)
+    tmpCoordinateArray = [queryEntry.Centroid' testEntry.Centroid'];
+    hankelWindowSize = calculateHankelWindowSize(tmpCoordinateArray);
+    kHankel = hankelConstruction(tmpCoordinateArray, hankelWindowSize);
+end
+
+function hankelWindowSize = calculateHankelWindowSize(inputFeature)
+    hankelWindowSize = ceil(size(inputFeature, 2) / 3);
+end
+
+
+function isDynamicsQualify = checkDynamics(S)
+    isDynamicsQualify = 0;
+    rankOrder = sum(S > mean(S) + std(S));
+    if rankOrder == 1
+        isDynamicsQualify = 1;
+    end
+end    
--- trajDebug.m
+++ trajDebug.m
@@ -3,8 +3,10 @@ main_class;
 trackBlobsObj = TrackBlobs(blobDetector.inputVideoData);
 trackBlobsObj.OpenClosingProcess();
 trackBlobsObj.blobTrackingFunc();
+
 trackBlobsObj.DBMergeLocation();
 trackBlobsObj.DBMergeLocation();
 % TODO Dynamics
+trackBlobsObj.DBMergeDynamics();
 trackBlobsObj.DBSortByFrame();
 trackBlobsObj.playTrackingBlobs();
