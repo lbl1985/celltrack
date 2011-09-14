b6adaf00bc467af84d2cd247527260b2034b5026
Binlong Li <herbert19lee@gmail.com>
2011-09-13 20:27:18 -0400

    one more clean up criteria for Time Unconsistent showing up trajectories.

--- 20Tracking/@TrackBlobs/dbCleanUp.m
+++ 20Tracking/@TrackBlobs/dbCleanUp.m
@@ -1,6 +1,7 @@
 function dbCleanUp(obj)
     cleanUpOnlyOneAppearance(obj);
     cleanUpSteady(obj);
+    cleanUpUnConsistent(obj);
 end
 
 function cleanUpOnlyOneAppearance(obj)
@@ -25,6 +26,21 @@ function cleanUpSteady(obj)
     end
     obj.DB = obj.DB(keepIndex);
 end
+
+function cleanUpUnConsistent(obj)
+    ndb = length(obj.DB);
+    keepIndex = true(ndb, 1);
+    for i = 1 : ndb
+        timeIDX = obj.DB{i}.timeIDX;
+        timeIDXDiff = timeIDX(2 : end) - timeIDX(1:end - 1);
+        if any(timeIDXDiff > 5) || sum(timeIDXDiff == 1) <= 3
+            keepIndex(i) = false;
+        end
+    end
+    obj.DB = obj.DB(keepIndex);
+end
+        
+    
 
 function isSteady = checkIsSteady(obj, i)
     tmpEntry = obj.DB{i};
