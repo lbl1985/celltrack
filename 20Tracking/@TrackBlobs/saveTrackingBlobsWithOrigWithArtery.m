fad54777d0f508e3ed4be5a6f39890e4db57718f
Binlong Li <herbert19lee@gmail.com>
2012-01-23 11:37:10 -0500

    save the video for original with artery image.

---
+++ 20Tracking/@TrackBlobs/saveTrackingBlobsWithOrigWithArtery.m
@@ -0,0 +1,29 @@
+function saveTrackingBlobsWithOrigWithArtery( obj, origVideo, artery )
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+
+rectShow = figure;
+
+saver1 = videoSaver(obj.videoName, 11);
+saver1.fig = rectShow;
+
+    for t = 1 : obj.fg.nFrame
+        artery_m = artery;
+        I = uint8(obj.fg.Data(:, :, t));
+        artery_m(I>0.01) = 255;
+%         I(I<=0.01) = 0;
+        %     combinedI = [origVideo(:, :, t) ones(size(origVideo, 1), 10) artery_m];
+        combinedI = [floor((origVideo(:, :, t)/2 + artery / 2)) ones(size(origVideo, 1), 10) artery_m];
+        columnOffSet = size(origVideo, 2) + 10;
+        imshow(combinedI, 'border', 'tight');
+        for j = 1 : length(obj.DBbyFrame{t})
+            tBlob = obj.DBbyFrame{t}(j);        
+            cellBoundingShow(tBlob, tBlob.ID, rectShow, columnOffSet);
+            obj.drawTrajectoryWithOrig(tBlob.ID, t, rectShow);
+        end
+        writenum2(t);
+        pause(1/22);
+        saver1.saveCore();
+    end
+    saver1.saveClose;
+end
