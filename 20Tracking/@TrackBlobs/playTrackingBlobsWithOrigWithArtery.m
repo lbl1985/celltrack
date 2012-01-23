e3c74b147e99831dc8bf12cb957a1721b6f2c9ee
Binlong Li <herbert19lee@gmail.com>
2012-01-23 11:19:42 -0500

    applying the artery back into the original video too.

--- 20Tracking/@TrackBlobs/playTrackingBlobsWithOrigWithArtery.m
+++ 20Tracking/@TrackBlobs/playTrackingBlobsWithOrigWithArtery.m
@@ -11,7 +11,8 @@ for t = 1 : obj.fg.nFrame
     I = uint8(obj.fg.Data(:, :, t));
     artery_m(I>0.01) = 255;
 %     artery_m(I<=0.01) = 0;
-    combinedI = [origVideo(:, :, t) ones(size(origVideo, 1), 10) artery_m];
+%     combinedI = [origVideo(:, :, t) ones(size(origVideo, 1), 10) artery_m];
+    combinedI = [floor((origVideo(:, :, t)/2 + artery / 2)) ones(size(origVideo, 1), 10) artery_m];
     columnOffSet = size(origVideo, 2) + 10;
 %     I = I > 0.01;
     imshow(combinedI, 'border', 'tight');
