d02991f8bff6c94536992d798075f5f9fb1cc9f8
Binlong Li <herbert19lee@gmail.com>
2012-04-02 17:18:34 -0400

    overlay multiple video tracking results on to one artery image.

--- 90support/drawAllTrajInOneImage.m
+++ 90support/drawAllTrajInOneImage.m
@@ -1,6 +1,10 @@
 function drawAllTrajInOneImage(trackBlobsObj, img)
 % Input:
-% trackBlobsObj: trackBlobsObjs from preocessed program
+% trackBlobsObj: 1. trackBlobsObjs from preocessed program
+%                2. trackBlobsObjs could be a cell array with each cell
+%                contains one trackBlobsObjs, which generate from program
+%                eg. trackBlobsObjs{1} = trackBlobsObjFromVideo1;
+%                    trackBlobsObjs{2} = trackBlobsObjFromVideo2; .. e.t.c
 % img:           Background(artery) image, on which to layout trajs
 % Binlong Li        27 March 2012
 
@@ -9,6 +13,13 @@ h = figure(5);
 img = imresize(uint16_2_uint8(img), [125 125]);
 imshow(img);
 
+if iscell(trackBlobsObj)
+    trackBlobsObjBackUp = trackBlobsObj;
+    trackBlobsObj.DB = [];
+    for i = 1 : length(trackBlobsObjBackUp)
+        trackBlobsObj.DB = cat(1, trackBlobsObj.DB, trackBlobsObjBackUp{i}.DB);
+    end
+end
 nCell = length(trackBlobsObj.DB);
 colors = varycolor(nCell);
 hold on;
